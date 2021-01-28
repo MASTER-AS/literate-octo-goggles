93263c48053896cd1a3f7f8f209b04e20e7dede5
MASTER ASK <69575627+MASTER-AS@users.noreply.github.com>
2021-01-28 04:28:11 +0000

    Pattern.py

---
+++ .github/workflows/Pattern.py
@@ -0,0 +1,22 @@
+#//'''''''''''''😁 ENTER A NUMBER 😁''''''''''//
+
+import java.util.Scanner; 
+public class Program{
+    public static void main(String[] args) {
+
+        Scanner ask = new Scanner(System.in);
+        int row = ask.nextInt();
+        int z;
+        int a;
+        for (z = 1; z <= row; ++z){
+            for (a = 1; a <= z; ++a){
+                System.out.print("💖");
+            }
+            System.out.println(" ");
+        }
+        
+        System.out.println("\n MADE BY ❤MASTER ASK❤");
+        
+        //Made By 💖Master ask💖
+    }
+}
